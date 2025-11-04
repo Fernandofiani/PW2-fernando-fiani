2e83723a75ad54c231b02e8b6787073d0fa4540b
Fernando Fiani <fernandonokou@gmail.com>
2025-11-04 15:48:04 -0300

    atividade de conexao ao banco

---
+++ Segundo Semestre/Aula-09/index.php
@@ -0,0 +1,19 @@
+<?php
+require 'databaseconnect.php';
+include 'inserir_dados.php';
+?>
+<html>
+        <head>
+        <link rel="stylesheet" href="tchetche.css">
+
+        </head>
+      <body>
+        <div class="container">
+        <h1 class="titulo">Você irá se cadastrar ou fazer o login?</h1>
+        <a href="login.html" class="botao">Login</a>
+        <a href="cadastro.html" class="botao">Cadastrar</a>
+        </div>
+
+
+      </body>
+</html>
